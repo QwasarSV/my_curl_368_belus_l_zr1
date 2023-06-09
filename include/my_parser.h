4a00fc79d12f838d39c308ceff5e70757fa4785c
LBelus <lorris.belus@icloud.com>
2023-06-09 11:33:14 +0000

    add url parser

--- include/my_parser.h
+++ include/my_parser.h
@@ -3,16 +3,25 @@
 
 #include <main_header.h>
 
+#define MAX_INPUT_TOKENS 20
+#define GET_MSG_P1 "GET "
+#define GET_MSG_P2 " HTTP/1.1\r\nHost:"
+#define GET_MSG_P3 "\r\nConnection: close\r\n\r\n"
+#define BASE_LENGTH 42
 
-#define __SPACE_CHAR__   ' '
-#define MAX_INPUT_TOKENS 250
-#define DELIMITER        '\n'
-#define BLOCK_SIZE       512
+#ifndef PARSED_URL
+#define PARSED_URL
+struct parsed_url_struct
+{
+  char* domain;
+  char* path;
+  char* get_request;
+};
+typedef struct parsed_url_struct s_parsed;
+#endif
 
-int     get_size_fd(char* filename);
-char*   read_map(char* filename);
-char**  dirty_split(char* str, char ch);
-int     find_ch(char* str, char ch);
-char**  split_env(char* str, char ch);
+char* create_get_request(char* path, char* domain, int total_str_len);
+s_parsed* my_urL_parser(char* url_list);
+
 
 #endif
---
+++ src/url_parser/url_parser.c
@@ -0,0 +1,44 @@
+#include <main_header.h>
+
+/*
+################ create_get_request #################
+# Creates a GET request for the provided path and domain.
+# @return {char*} A pointer to the newly created GET request string.
+*/
+char* create_get_request(char* path, char* domain, int total_str_len)
+{
+  int len_req = BASE_LENGTH + total_str_len;
+  char* new_req = malloc(sizeof(char) * len_req + 1);
+  my_bzero(new_req, len_req + 1);
+  my_strcat(new_req, GET_MSG_P1);
+  my_strcat(new_req, path);
+  my_strcat(new_req, GET_MSG_P2);
+  my_strcat(new_req, domain);
+  my_strcat(new_req, GET_MSG_P3);
+  return new_req;
+}
+
+/*
+################ my_urL_parser #################
+# Parses a URL into its domain and path components, and then creates a GET request.
+# @return {s_parsed*} A pointer to the parsed URL structure, which contains the domain, path, and GET request.
+*/
+s_parsed* my_urL_parser(char* url_list)
+{
+  s_parsed* url = malloc(sizeof(s_parsed));
+  char* ptr_doma = NULL;
+  char* ptr_path = NULL;
+  ptr_doma  = my_strstr(url_list, "://");
+  ptr_path = my_strchr(&ptr_doma[3],'/');
+  int len_dom = (int)(ptr_path - &ptr_doma[3]);
+  int len_path = my_strlen(ptr_path);
+  int len = len_dom + len_path;
+  url->domain = malloc(sizeof(char)*len_dom + 1);
+  my_bzero(url->domain, len_dom + 1);
+  my_memcpy(url->domain, &ptr_doma[3], len_dom);
+  url->path = malloc(sizeof(char)*len_path + 1);
+  my_bzero(url->path, len_path + 1);
+  my_memcpy(url->path, ptr_path, len_path);
+  url->get_request = create_get_request(url->path, url->domain, len);
+  return url;
+}
