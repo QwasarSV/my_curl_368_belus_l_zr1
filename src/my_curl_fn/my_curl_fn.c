43b9a80b98e71b347869d217f0b710b4d12e6442
lorris belus <belus_l@qwasar.io>
2023-06-12 14:24:24 +0000

    modify a comment that was triggering Gandalf macro safeguard

--- src/my_curl_fn/my_curl_fn.c
+++ src/my_curl_fn/my_curl_fn.c
@@ -20,16 +20,17 @@ int send_get_req(char* get, int sockfd)
   return EXIT_SUCCESS;
 }
 
-/*
-################ r_socket_w_out #################
-# Reads data from a socket, identified by the sockfd parameter, and writes the read data to the standard output.
-# Once all the data is read, the socket is closed.
-# @return {void}
-*/
+// /*
+// ################ r_socket_w_out #################
+// # Reads data from a socket, identified by the sockfd parameter, and writes the read data to the standard output.
+// # Once all the data is read, the socket is closed.
+// # @return {void}
+// */
 void r_socket_w_out(int sockfd)
 {
   char buffer[512];
   my_memset(buffer, 0, sizeof(buffer));
+  
   while (read(sockfd, buffer, sizeof(buffer)-1) > 0)
   {
     write(STDOUT_FILENO, buffer, sizeof(buffer)-1);
@@ -38,12 +39,12 @@ void r_socket_w_out(int sockfd)
   close(sockfd);
 }
 
-/*
-################ connect_to_server #################
-# Connects a socket, identified by the sockfd parameter, to a server defined by the servinfo parameter.
-# If the connection fails, the function prints an error message, frees the servinfo structure, and closes the socket.
-# @return {int} Returns EXIT_SUCCESS if the socket is connected successfully, otherwise returns EXIT_FAILURE.
-*/
+// /*
+// ################ connect_to_server #################
+// # Connects a socket, identified by the sockfd parameter, to a server defined by the servinfo parameter.
+// # If the connection fails, the function prints an error message, frees the servinfo structure, and closes the socket.
+// # @return {int} Returns EXIT_SUCCESS if the socket is connected successfully, otherwise returns EXIT_FAILURE.
+// */
 int connect_to_server(int sockfd, struct addrinfo* servinfo)
 {
   if (connect(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) == -1)
@@ -56,12 +57,12 @@ int connect_to_server(int sockfd, struct addrinfo* servinfo)
   return EXIT_SUCCESS;
 }
 
-/*
-################ perform_get_request #################
-# Performs a GET request on the specified domain with the provided GET request string.
-# The response from the server is printed to the standard output.
-# @return {int} Returns 0 after successfully executing the function.
-*/
+// /*
+// ################ perform_get_request #################
+// # Performs a GET request on the specified domain with the provided GET request string.
+// # The response from the server is printed to the standard output.
+// # @return {int} Returns 0 after successfully executing the function.
+// */
 int perform_get_request(char* domain,  char* get)
 {
   int sockfd;
